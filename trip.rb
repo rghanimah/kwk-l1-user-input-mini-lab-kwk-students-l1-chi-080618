23e0c146c4c0ef774b5d7f2f64f39cd7535a85be
rghanimah <rabiaghanimah@gmail.com>
2018-08-07 14:54:00 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -1,7 +1,9 @@
 # Code your prompts here!
-
+city = #get.chomp
+puts "what city do you want to visit?"
 # First, puts out a string asking where the tourist would like to stay.
 puts "Let's plan your trip! Where would you like to stay?"
+#stay = gets.chomp 
 
 # Then, set a variable called stay to get the user's input. Make sure to capitilize that input.
 
